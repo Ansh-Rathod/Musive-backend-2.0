067e3dbddaf36655947c6a17ab7c8a5b44a9423a
Ansh-Rathod <anshrathod29@gmail.com>
2022-11-05 11:22:29 +0530

    changed cors config

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../../.env" });
 
-const corsConfig = {
-  origin: "https://musive.vercel.app",
+var whitelist = [
+  "https://musive.vercel.app",
+  "http://localhost:3000",
+  "http://192.168.43.163:3000",
+];
+var corsConfig = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
 

1a81fe11fd8b07eb70cc43f46914d593f284a511
Ansh-Rathod <anshrathod29@gmail.com>
2022-11-05 10:02:19 +0530

    removed dist folder

--- dist/methods/async-function.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-exports.default = asyncHandler;

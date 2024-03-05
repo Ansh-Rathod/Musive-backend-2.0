a5bef1ebdabf0e17d9e9ec147d18dd211598323e
Ansh rathod <anshrathod29@gmail.com>
2024-03-05 11:10:46 +0530

    adding new cors

--- src/index.js
+++ src/index.js
@@ -21,6 +21,15 @@ const PORT = process.env.PORT || 4444;
 
 app.use(helmet());
 app.use(cors());
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
